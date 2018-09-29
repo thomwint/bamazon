7a0d9d78c0580cce7324b99c974f3569416fe4af
Thomas Winters <winterst85@gmail.com>
2018-09-29 17:28:38 -0500

    added functionality for when there is enough in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -70,21 +70,35 @@ const customerQuestions = () => {
     ])
     .then(answer => {
       let query =
-        "SELECT id, product_name, stock_quantity FROM products WHERE ?";
+        "SELECT id, product_name, stock_quantity, price FROM products WHERE ?";
       connection.query(query, { id: answer.product }, function(err, res) {
         if (err) throw err;
         //Check if there are less in stock than customer wants
         if (answer.quantity > res[0].stock_quantity) {
           console.log(
-            `\n**Insufficient quantity! There are only ${
+            `\n**Insufficient quantity! There are ${
               res[0].stock_quantity
-            } in stock for ${res[0].product_name}**\n`
+            } left in stock for ${res[0].product_name}**\n`
+            
           );
-          customerQuestions();
-        } else {
+        } 
+        else {
           //if enough, update the customer what and how much they bought and the update the database,
           //then display the inventory, ask questions
+          console.log(
+            `Your purchased ${answer.quantity} of ${res[0].product_name}`
+          );
+          console.log(
+            `Your purchase totaled: ${res[0].price * answer.quantity}`
+          );
+          const sql =
+            `UPDATE products SET stock_quantity = ${res[0].stock_quantity - answer.quantity} WHERE id = ${answer.product}`;
+          connection.query(sql, function(err, result) {
+            if (err) console.log(err);
+          });
         }
+        customerQuestions();
       });
+      
     });
 };
